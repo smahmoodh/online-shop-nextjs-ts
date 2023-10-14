8c3ced10eea606807e57f1e9cf07e2732807210c
S.M.H <seyedmahmoodhosseini@gmail.com>
2023-10-14 17:46:43 +0330

    replace class with className in loading

--- app/loading.js
+++ app/loading.js
@@ -4,15 +4,15 @@ const loading = () => {
   return (
     // <ProgressBar />
     <>
-      <div class="absolute top-0 left-0 w-full h-full bg-white/[.5] rounded-md dark:bg-gray-800/[.4] z-[1000]"></div>
+      <div className="absolute top-0 left-0 w-full h-full bg-white/[.5] rounded-md dark:bg-gray-800/[.4] z-[1000]"></div>
 
-      <div class="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-[1001]">
+      <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-[1001]">
         <div
-          class="animate-spin inline-block w-6 h-6 border-[3px] border-current border-t-transparent text-blue-600 rounded-full"
+          className="animate-spin inline-block w-6 h-6 border-[3px] border-current border-t-transparent text-blue-600 rounded-full"
           role="status"
           aria-label="loading"
         >
-          <span class="sr-only">Loading...</span>
+          <span className="sr-only">Loading...</span>
         </div>
       </div>
     </>
